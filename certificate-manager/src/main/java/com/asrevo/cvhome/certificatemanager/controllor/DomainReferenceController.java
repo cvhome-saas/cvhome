2c6cc26667f0c5379820d618e03a47399e78a865
ashraf <ashraf1abdelrasool@gmail.com>
2023-08-19 00:16:54 +0300

    fix url

--- certificate-manager/src/main/java/com/asrevo/cvhome/certificatemanager/controllor/DomainReferenceController.java
+++ certificate-manager/src/main/java/com/asrevo/cvhome/certificatemanager/controllor/DomainReferenceController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/domain-reference")
+@RequestMapping("api/v1/domain-reference")
 @AllArgsConstructor
 @Slf4j
 public class DomainReferenceController {
