2f710061e2e0ef6fc0ffa894b8fdde643c0c91f5
ashraf <ashraf1abdelrasool@gmail.com>
2023-08-19 15:15:10 +0300

    add csrf token repository add gateway to local microservices gateway

--- gateway/src/main/java/com/asrevo/cvhome/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/asrevo/cvhome/gateway/config/SecurityConfig.java
@@ -1,20 +1,42 @@
 package com.asrevo.cvhome.gateway.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher.MatchResult.match;
+import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher.MatchResult.notMatch;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(it -> it.anyExchange().authenticated())
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, CookieServerCsrfTokenRepository tokenRepository) {
+        return http.authorizeExchange(it -> it.anyExchange().permitAll())
                 .oauth2Login(withDefaults())
                 .oauth2Client(withDefaults())
+                .csrf(it -> it.csrfTokenRepository(tokenRepository).requireCsrfProtectionMatcher(SecurityConfig::matches))
                 .build();
     }
+
+    @Bean
+    public CookieServerCsrfTokenRepository cookieServerCsrfTokenRepository() {
+        return CookieServerCsrfTokenRepository.withHttpOnlyFalse();
+    }
+
+    private static Mono<ServerWebExchangeMatcher.MatchResult> matches(ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        return (request.getMethod() != HttpMethod.GET && !request.getPath().toString().startsWith("/auth")) ? match() : notMatch();
+    }
+
+
 }
