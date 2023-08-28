f46678df8c675a21418d33ffb198d8a0ba9d25ac
ashraf <ashraf1abdelrasool@gmail.com>
2023-08-28 10:22:33 +0300

    update domain-reference with new filds

--- ui/src/app/model/domain-reference.ts
+++ ui/src/app/model/domain-reference.ts
@@ -1,12 +1,18 @@
 export interface DomainReference {
   id?: number,
   domain: string,
-  domainType?: DomainType;
   reference?: string,
+  domainType?: DomainType;
+  domainStatus?: DomainStatus;
+  createdDate?: Date,
 }
 
 export enum DomainType {
   APPLICATION = 'APPLICATION',
   SUB = 'SUB',
   CUSTOM = 'CUSTOM'
+}
+
+export enum DomainStatus {
+  INITIATED = 'INITIATED', ATTACHED = 'ATTACHED', UNATTACHED = 'UNATTACHED'
 }
