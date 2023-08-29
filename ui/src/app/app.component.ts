418f8ff1bb154560ed22a0a28c6964ee5c52d08e
ashraf <ashraf1abdelrasool@gmail.com>
2023-08-29 12:35:25 +0300

    remove user domain

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {User} from "./domain/user";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,6 @@ import {User} from "./domain/user";
 })
 export class AppComponent implements OnInit {
   title = 'ui';
-  user: User | undefined;
 
   constructor() {
   }
