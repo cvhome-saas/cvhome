fce9435b601e6adf495af1e91ecab14b47672376
ashraf <ashraf1abdelrasool@gmail.com>
2023-09-02 02:28:26 +0300

    remove un needed replace

--- commons/src/main/java/com/asrevo/cvhome/commons/domain/DomainCertificateOrder.java
+++ commons/src/main/java/com/asrevo/cvhome/commons/domain/DomainCertificateOrder.java
@@ -2,7 +2,6 @@ package com.asrevo.cvhome.commons.domain;
 
 import com.asrevo.cvhome.commons.event.Event;
 import com.asrevo.cvhome.commons.event.SimpleEvent;
-import com.asrevo.cvhome.commons.utils.Defines;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +13,10 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static com.asrevo.cvhome.commons.utils.Defines.OrderEvents.*;
 
 
 @Getter
@@ -31,25 +33,29 @@ public class DomainCertificateOrder extends AbstractAggregateRoot<DomainCertific
     private CertificateOrderStatus certificateOrderStatus;
     private Challenges challenges;
 
-    public static DomainCertificateOrder newOrder() {
-        DomainCertificateOrder domainCertificateOrder = new DomainCertificateOrder();
-        domainCertificateOrder.registerEvent(new SimpleEvent(Defines.OrderEvents.DOMAIN_CERTIFICATE_ORDER_CREATED, new HashMap<>()));
-        return domainCertificateOrder;
-    }
 
     @Override
     protected Collection<Object> domainEvents() {
         return super.domainEvents().stream().peek(it -> {
             if (it instanceof Event event) {
                 if (id != null) {
-                    event.data().put("id", id.toString());
+                    event.data().put(DOMAIN_CERTIFICATE_ORDER_ID_KEY, id.toString());
                 }
             }
         }).collect(Collectors.toList());
     }
 
-    public void setCertificateOrderStatus(CertificateOrderStatus certificateOrderStatus) {
-        this.certificateOrderStatus = certificateOrderStatus;
-        this.registerEvent(new SimpleEvent(Defines.OrderEvents.DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED, new HashMap<>()));
+    public static DomainCertificateOrder newOrder() {
+        DomainCertificateOrder domainCertificateOrder = new DomainCertificateOrder();
+        domainCertificateOrder.registerEvent(new SimpleEvent(DOMAIN_CERTIFICATE_ORDER_CREATED, new HashMap<>()));
+        return domainCertificateOrder;
+    }
+
+    public void setCertificateOrderStatus(CertificateOrderStatus status) {
+        this.certificateOrderStatus = status;
+        if (status != null) {
+            Map<String, String> data = Map.of(DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED_KEY, status.name());
+            this.registerEvent(new SimpleEvent(DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED_EVENT, new HashMap<>(data)));
+        }
     }
 }
--- commons/src/main/java/com/asrevo/cvhome/commons/utils/Defines.java
+++ commons/src/main/java/com/asrevo/cvhome/commons/utils/Defines.java
@@ -5,6 +5,8 @@ public class Defines {
 
     public static class OrderEvents {
         public static final String DOMAIN_CERTIFICATE_ORDER_CREATED = "DOMAIN_CERTIFICATE_ORDER_CREATED";
-        public static final String DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED = "DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED";
+        public static final String DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED_EVENT = "DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED";
+        public static final String DOMAIN_CERTIFICATE_ORDER_ID_KEY = "ID";
+        public static final String DOMAIN_CERTIFICATE_ORDER_STATUS_CHANGED_KEY = "EVENT";
     }
 }
