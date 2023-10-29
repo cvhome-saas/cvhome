fee8ca5d6178301986bad3bbeba0e73d7f101411
ashraf <ashraf1abdelrasool@gmail.com>
2023-10-29 16:06:54 +0200

    adding route53 installer support multiple domains for single order

--- domain-certificate-manager/domain-certificate-manager-commons/src/main/java/com/asrevo/cvhome/domaincertificatemanager/commons/dto/RegisteredDomainResponse.java
+++ domain-certificate-manager/domain-certificate-manager-commons/src/main/java/com/asrevo/cvhome/domaincertificatemanager/commons/dto/RegisteredDomainResponse.java
@@ -6,6 +6,6 @@ import com.asrevo.cvhome.domaincertificatemanager.commons.domain.DomainType;
 import com.asrevo.cvhome.domaincertificatemanager.commons.domain.Reference;
 
 public record RegisteredDomainResponse(Domain domain, DomainType domainType, Reference reference,
-                                       DomainCertificateStatus certificateStatus) {
+                                       DomainCertificateStatus status) {
 
 }
