9b2d4cee454a1e7685cf227d13a84b5edf598a1d
ashraf <ashraf1abdelrasool@gmail.com>
2023-11-19 14:29:59 +0200

    adding fargate-task-info module

--- domain-certificate-manager/domain-certificate-manager-service/src/main/java/com/asrevo/cvhome/domaincertificatemanager/config/EcsTaskHealthIndicator.java
+++ domain-certificate-manager/domain-certificate-manager-service/src/main/java/com/asrevo/cvhome/domaincertificatemanager/config/EcsTaskHealthIndicator.java
@@ -3,6 +3,7 @@ package com.asrevo.cvhome.domaincertificatemanager.config;
 import com.asrevo.cvhome.fargate.task.EcsTask;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
+@Slf4j
 public class EcsTaskHealthIndicator implements HealthIndicator {
     private final EcsTask ecsTask;
     private final ObjectMapper objectMapper;
@@ -28,6 +30,7 @@ public class EcsTaskHealthIndicator implements HealthIndicator {
                     }));
         } catch (Exception e) {
             status = Health.down(e);
+            log.error("error EcsTaskHealthIndicator", e);
         }
         return status.build();
     }
