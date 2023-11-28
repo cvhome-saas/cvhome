195534d4ea43a0d7260a3b8ed601fa69c9179f45
ashraf <ashraf1abdelrasool@gmail.com>
2023-11-28 20:07:18 +0200

    try to call dns to resolve auth

--- gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/config/WebClientServicesConfig.java
+++ gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/config/WebClientServicesConfig.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2Clien
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.WebClientReactivePasswordTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.WebClientReactiveRefreshTokenTokenResponseClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -102,5 +103,12 @@ public class WebClientServicesConfig {
         return defaultReactiveOAuth2UserService;
     }
 */
+
+    @Bean
+    public WebClientReactiveAuthorizationCodeTokenResponseClient webClientReactiveAuthorizationCodeTokenResponseClient(WebClient.Builder defaultMicroServiceBuilder) {
+        WebClientReactiveAuthorizationCodeTokenResponseClient webClientReactiveAuthorizationCodeTokenResponseClient = new WebClientReactiveAuthorizationCodeTokenResponseClient();
+        webClientReactiveAuthorizationCodeTokenResponseClient.setWebClient(defaultMicroServiceBuilder.build());
+        return webClientReactiveAuthorizationCodeTokenResponseClient;
+    }
 
 }
