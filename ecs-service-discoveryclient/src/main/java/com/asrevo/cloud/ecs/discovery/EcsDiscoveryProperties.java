bcf7ad74bbe3e81232dc030a1a63ddbac59f66b5
ashraf <ashraf1abdelrasool@gmail.com>
2023-12-01 22:26:44 +0200

    Revert "try to call dns to resolve auth"
    This reverts commit 5c080117864e4080e25d5d342817532427f629ac.

---
+++ ecs-service-discoveryclient/src/main/java/com/asrevo/cloud/ecs/discovery/EcsDiscoveryProperties.java
@@ -0,0 +1,20 @@
+package com.asrevo.cloud.ecs.discovery;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ConfigurationProperties(EcsDiscoveryProperties.PREFIX)
+@Getter
+@Setter
+public class EcsDiscoveryProperties {
+    public static final String PREFIX = "spring.cloud.ecs.discovery";
+    private String namespace;
+    private String namespaceId;
+    private boolean enabled = true;
+    private Integer defaultPort = 8080;
+    private Map<String, Integer> servicePorts = new HashMap<>();
+}
