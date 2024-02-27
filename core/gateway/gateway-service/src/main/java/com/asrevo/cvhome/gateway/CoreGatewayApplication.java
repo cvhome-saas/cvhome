c348e68c57c4435e669c88d4c6039a646a0ed66d
as-mahmoud <asmahmoud@unifonic.com>
2024-02-27 18:13:57 +0200

    change arch & add servics

--- core/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/CoreGatewayApplication.java
+++ core/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/CoreGatewayApplication.java
@@ -1,12 +1,13 @@
 package com.asrevo.cvhome.gateway;
 
 import com.asrevo.cvhome.gateway.config.FargateProperties;
+import com.asrevo.cvhome.gateway.config.ServiceDomainProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {FargateProperties.class})
+@EnableConfigurationProperties(value = {FargateProperties.class, ServiceDomainProperties.class})
 public class CoreGatewayApplication {
   @lombok.Generated
   public static void main(String[] args) {
--- saas/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/SaasGatewayApplication.java
+++ saas/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/SaasGatewayApplication.java
@@ -1,13 +1,14 @@
 package com.asrevo.cvhome.gateway;
 
 import com.asrevo.cvhome.gateway.config.FargateProperties;
+import com.asrevo.cvhome.gateway.config.ServiceDomainProperties;
 import com.asrevo.cvhome.gateway.config.ssl.SslProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {SslProperties.class, FargateProperties.class})
+@EnableConfigurationProperties(value = {SslProperties.class, FargateProperties.class, ServiceDomainProperties.class})
 public class SaasGatewayApplication {
     @lombok.Generated
     public static void main(String[] args) {
