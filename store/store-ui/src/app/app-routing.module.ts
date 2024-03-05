8d16c321256da36e1c780a095b68dc9ffa2cf95c
as-mahmoud <asmahmoud@unifonic.com>
2024-03-05 03:02:51 +0200

    change arch & add servics

--- store/store-ui/src/app/app-routing.module.ts
+++ store/store-ui/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FirstComponent} from "./views/first/first.component";
+import {SecondComponent} from "./views/second/second.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first', component: FirstComponent},
+  {path: 'second', component: SecondComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking'
-})],
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
