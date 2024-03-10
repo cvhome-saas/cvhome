aaf23b63364ecc68f371f8b1f5e7dabc8aa02f30
as-mahmoud <asmahmoud@unifonic.com>
2024-03-10 20:08:37 +0200

    change arch & add servics

--- store-core/wallet/wallet-service/src/main/java/com/asrevo/cvhome/wallet/WalletApplication.java
+++ store-core/wallet/wallet-service/src/main/java/com/asrevo/cvhome/wallet/WalletApplication.java
@@ -1,16 +1,17 @@
 package com.asrevo.cvhome.wallet;
 
+import com.asrevo.cvhome.wallet.config.ServiceDomainProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({})
+@EnableConfigurationProperties({ServiceDomainProperties.class})
 public class WalletApplication {
 
-  @lombok.Generated
-  public static void main(String[] args) {
-    SpringApplication.run(WalletApplication.class, args);
-  }
+    @lombok.Generated
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class, args);
+    }
 
 }
