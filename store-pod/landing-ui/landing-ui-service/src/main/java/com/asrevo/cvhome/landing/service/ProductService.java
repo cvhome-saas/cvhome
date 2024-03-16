eb3bbb4150246278b17a215d16651e7770917753
as-mahmoud <asmahmoud@unifonic.com>
2024-03-16 21:43:15 +0200

    change arch & add services

--- store-pod/landing-ui/landing-ui-service/src/main/java/com/asrevo/cvhome/landing/service/ProductService.java
+++ store-pod/landing-ui/landing-ui-service/src/main/java/com/asrevo/cvhome/landing/service/ProductService.java
@@ -1,11 +1,11 @@
 package com.asrevo.cvhome.landing.service;
 
 
+import com.asrevo.cvhome.commons.utils.CustomPageImpl;
 import com.asrevo.cvhome.store.commons.domain.StoreId;
 import com.asrevo.cvhome.storepod.commons.domain.ProductId;
 import com.asrevo.cvhome.storepod.commons.dto.DetailedProductDto;
 import com.asrevo.cvhome.storepod.commons.dto.ProductDto;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -17,5 +17,5 @@ public interface ProductService {
     DetailedProductDto getDetailedProduct(StoreId storeId, ProductId productId);
 
     @PostExchange("find-all")
-    Page<ProductDto> findAll(StoreId storeId, PageRequest pageable);
+    CustomPageImpl<ProductDto> findAll(StoreId storeId, PageRequest pageable);
 }
--- store-pod/landing-ui/landing-ui-service/src/test/java/com/asrevo/cvhome/landing/TestLandingApplication.java
+++ store-pod/landing-ui/landing-ui-service/src/test/java/com/asrevo/cvhome/landing/TestLandingApplication.java
@@ -1,14 +1,9 @@
 package com.asrevo.cvhome.landing;
 
-import com.asrevo.cvhome.landing.service.CategoryService;
-import com.asrevo.cvhome.landing.service.ProductService;
-import com.asrevo.cvhome.store.commons.domain.StoreId;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.PageRequest;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -23,17 +18,5 @@ public class TestLandingApplication {
 
     public static void main(String[] args) {
         SpringApplication.from(LandingApplication::main).with(TestLandingApplication.class).run(args);
-    }
-
-    @Bean
-    public CommandLineRunner runner(ProductService productService, CategoryService categoryService) {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-                categoryService.getStoreCategoriesView(StoreId.newId());
-                productService.findAll(StoreId.newId(), PageRequest.of(2, 9));
-//                productService.findAll(Utils.serialize(StoreId.newId()), PageRequest.of(0, 10));
-            }
-        };
     }
 }
