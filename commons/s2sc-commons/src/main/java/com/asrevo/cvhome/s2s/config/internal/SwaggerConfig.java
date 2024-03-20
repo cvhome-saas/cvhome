ac936e941a0cdc31932dd8957b12ba5f8a371b8f
as-mahmoud <asmahmoud@unifonic.com>
2024-03-20 20:11:36 +0200

    change arch & add services

--- commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/config/internal/SwaggerConfig.java
+++ commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/config/internal/SwaggerConfig.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(OpenAPI.class)
 public class SwaggerConfig {
 
     @Bean
--- commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/config/internal/WebConfig.java
+++ commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/config/internal/WebConfig.java
@@ -1,11 +1,14 @@
 package com.asrevo.cvhome.s2s.config.internal;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
 @Configuration
+@ConditionalOnClass(Pageable.class)
 public class WebConfig implements WebFluxConfigurer {
     @Override
     public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
