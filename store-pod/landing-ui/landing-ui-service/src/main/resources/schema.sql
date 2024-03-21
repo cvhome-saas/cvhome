699f5d97f7262e505263f826dbba802ccc3f03bd
as-mahmoud <asmahmoud@unifonic.com>
2024-03-21 17:52:03 +0200

    change arch & add services

--- store-pod/landing-ui/landing-ui-service/src/main/resources/schema.sql
+++ store-pod/landing-ui/landing-ui-service/src/main/resources/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS landing.store
 
 CREATE TABLE IF NOT EXISTS landing.owner_store_ref
 (
-    owner_id  varchar(50) not null,
+    owner_id varchar(50) not null,
     store_id varchar(24) not null,
     constraint owner_store_ref_owner_id_fk foreign key (owner_id) references owner,
     constraint owner_store_ref_store_id_fk foreign key (store_id) references store
