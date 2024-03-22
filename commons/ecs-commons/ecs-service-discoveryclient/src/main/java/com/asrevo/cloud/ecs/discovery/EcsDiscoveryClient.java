8382c3e71b2c681d8bf6d3831b5028440f2c4271
as-mahmoud <asmahmoud@unifonic.com>
2024-03-22 20:47:30 +0200

    change arch & add services

---
+++ commons/ecs-commons/ecs-service-discoveryclient/src/main/java/com/asrevo/cloud/ecs/discovery/EcsDiscoveryClient.java
@@ -0,0 +1,93 @@
+package com.asrevo.cloud.ecs.discovery;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import software.amazon.awssdk.services.servicediscovery.ServiceDiscoveryClient;
+import software.amazon.awssdk.services.servicediscovery.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class EcsDiscoveryClient implements DiscoveryClient {
+    private final EcsDiscoveryProperties properties;
+    private final ServiceDiscoveryClient discovery;
+
+    public EcsDiscoveryClient(EcsDiscoveryProperties properties, ServiceDiscoveryClient discovery) {
+        this.properties = properties;
+        this.discovery = discovery;
+    }
+
+    public static List<ServiceInstance> getDefaultServiceInstances(ServiceDiscoveryClient discovery,
+                                                                   EcsDiscoveryProperties properties,
+                                                                   String serviceId) {
+        log.info("getting services for {}", serviceId);
+        DiscoverInstancesRequest request = DiscoverInstancesRequest
+                .builder()
+                .namespaceName(extractNamespace(serviceId).orElse(properties.getNamespace()))
+                .serviceName(extractServiceName(serviceId).orElse(serviceId)).build();
+        DiscoverInstancesResponse discoverInstancesResponse = discovery.discoverInstances(request);
+        List<HttpInstanceSummary> instances = discoverInstancesResponse.instances();
+        log.info("getting {} services for {}", instances.size(), serviceId);
+        return instances
+                .stream()
+                .map(instance -> {
+                    Integer defaultPort = properties.getServicePorts().getOrDefault(serviceId, properties.getDefaultPort());
+                    return (ServiceInstance) new CloudMapServiceInstance(instance, defaultPort);
+                })
+                .toList();
+    }
+
+    private static Optional<String> extractNamespace(String serviceId) {
+        int firstSplitter = serviceId.indexOf(".");
+        if (firstSplitter > 0 && serviceId.length() > firstSplitter + 1) {
+            return Optional.of(serviceId.substring(firstSplitter + 1));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<String> extractServiceName(String serviceId) {
+        int firstSplitter = serviceId.indexOf(".");
+        if (firstSplitter > 0) {
+            return Optional.of(serviceId.substring(0, firstSplitter));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static List<String> getEcsServices(ServiceDiscoveryClient discovery,
+                                              EcsDiscoveryProperties properties) {
+        log.info("getting all services");
+        List<ServiceFilter> filters = new ArrayList<>();
+        if (properties.getNamespaceId() != null) {
+            log.info("will apply namespace id filter for {}", properties.getNamespaceId());
+            filters.add(ServiceFilter.builder().name("NAMESPACE_ID").values(properties.getNamespaceId()).build());
+        }
+        ListServicesRequest servicesRequest = ListServicesRequest.builder().filters(filters).build();
+        ListServicesResponse listServicesResponse = discovery.listServices(servicesRequest);
+        List<ServiceSummary> services = listServicesResponse.services();
+        List<String> list = services.stream().map(ServiceSummary::name).toList();
+        ArrayList<String> result = new ArrayList<>(list);
+        result.addAll(properties.getIncludeServices());
+        return result;
+    }
+
+    @Override
+    public String description() {
+        return "ecs discovery client";
+    }
+
+    @Override
+    public List<ServiceInstance> getInstances(String serviceId) {
+        return getDefaultServiceInstances(this.discovery, this.properties, serviceId);
+    }
+
+    @Override
+    public List<String> getServices() {
+        return getEcsServices(this.discovery, this.properties);
+    }
+}
+
--- commons/ecs-commons/ecs-service-discoveryclient/src/main/java/com/asrevo/cloud/local/discovery/LocalDiscoveryConfig.java
+++ commons/ecs-commons/ecs-service-discoveryclient/src/main/java/com/asrevo/cloud/local/discovery/LocalDiscoveryConfig.java
@@ -3,11 +3,17 @@ package com.asrevo.cloud.local.discovery;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.cloud.client.ConditionalOnDiscoveryHealthIndicatorEnabled;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.client.discovery.health.DiscoveryClientHealthIndicatorProperties;
 import org.springframework.cloud.client.discovery.health.reactive.ReactiveDiscoveryClientHealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.REACTIVE;
+import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.SERVLET;
 
 /**
  * @author ashraf
@@ -22,6 +28,7 @@ public class LocalDiscoveryConfig {
      */
     @ConditionalOnLocalDiscoveryEnabled
     @Configuration
+    @ConditionalOnWebApplication(type = REACTIVE)
     public static class LocalReactiveDiscoveryClientAutoConfiguration {
 
         /**
@@ -32,20 +39,8 @@ public class LocalDiscoveryConfig {
          */
         @Bean
         @ConditionalOnMissingBean
-        public LocalReactiveDiscoveryClient reactiveDiscoveryClient(LocalDiscoveryProperties localDiscoveryProperties) {
+        public ReactiveDiscoveryClient reactiveDiscoveryClient(LocalDiscoveryProperties localDiscoveryProperties) {
             return new LocalReactiveDiscoveryClient(localDiscoveryProperties);
-        }
-
-        /**
-         * used to define bean for LocalDiscoveryClient
-         *
-         * @param localDiscoveryProperties used to configure the client
-         * @return LocalDiscoveryClient bean
-         */
-        @Bean
-        @ConditionalOnMissingBean
-        public LocalDiscoveryClient discoveryClient(LocalDiscoveryProperties localDiscoveryProperties) {
-            return new LocalDiscoveryClient(localDiscoveryProperties);
         }
 
         /**
@@ -58,9 +53,43 @@ public class LocalDiscoveryConfig {
         @Bean
         @ConditionalOnClass(name = "org.springframework.boot.actuate.health.ReactiveHealthIndicator")
         @ConditionalOnDiscoveryHealthIndicatorEnabled
-        public ReactiveDiscoveryClientHealthIndicator localReactiveDiscoveryClientHealthIndicator(LocalReactiveDiscoveryClient client, DiscoveryClientHealthIndicatorProperties properties) {
+        public ReactiveDiscoveryClientHealthIndicator localReactiveDiscoveryClientHealthIndicator(ReactiveDiscoveryClient client, DiscoveryClientHealthIndicatorProperties properties) {
             return new ReactiveDiscoveryClientHealthIndicator(client, properties);
         }
+    }
+
+    @ConditionalOnLocalDiscoveryEnabled
+    @Configuration
+    @ConditionalOnWebApplication(type = SERVLET)
+    public static class LocalDiscoveryClientAutoConfiguration {
+
+        /**
+         * used to define bean for LocalDiscoveryClient
+         *
+         * @param localDiscoveryProperties used to configure the client
+         * @return LocalDiscoveryClient bean
+         */
+        @Bean
+        @ConditionalOnMissingBean
+        public DiscoveryClient discoveryClient(LocalDiscoveryProperties localDiscoveryProperties) {
+            return new LocalDiscoveryClient(localDiscoveryProperties);
+        }
+
+        /**
+         * used to define bean for ReactiveDiscoveryClientHealthIndicator
+         *
+         * @param client     use client to do health check
+         * @param properties used to get current properties
+         * @return ReactiveDiscoveryClientHealthIndicator bean
+         */
+/*
+        @Bean
+        @ConditionalOnClass(name = "org.springframework.boot.actuate.health.HealthIndicator")
+        @ConditionalOnDiscoveryHealthIndicatorEnabled
+        public DiscoveryClientHealthIndicator localReactiveDiscoveryClientHealthIndicator(ObjectProvider<DiscoveryClient> client, DiscoveryClientHealthIndicatorProperties properties) {
+            return new DiscoveryClientHealthIndicator(client, properties);
+        }
+*/
     }
 
 }
