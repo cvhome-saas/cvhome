783c614b749f28da9fe54cdcd1f68cc0c3fa86cf
as-mahmoud <asmahmoud@unifonic.com>
2024-03-23 00:31:55 +0200

    change arch & add services

--- commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/model/ServiceDomain.java
+++ commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/model/ServiceDomain.java
@@ -1,11 +1,12 @@
 package com.asrevo.cvhome.s2s.model;
 
-public record ServiceDomain(String name, String domain, String port, String schema, String namespace,String gatewayServiceName) {
+public record ServiceDomain(String name, String domain, String port, String schema, String namespace,
+                            String gatewayServiceName) {
     public String getServiceHost() {
         return schema() + "://" + domain() + ":" + port();
     }
 
     public String getServiceHost(String service) {
-        return schema() + "://" + domain() + ":" + port() + "/" + service;
+        return schema() + "://" + domain() + ":" + port() + "/" + service + "/";
     }
 }
