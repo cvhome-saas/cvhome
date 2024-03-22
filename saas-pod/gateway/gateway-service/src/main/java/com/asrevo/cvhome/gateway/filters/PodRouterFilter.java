419bc6c62e2e53ee39d7dbcfb669fc47fd35af17
as-mahmoud <asmahmoud@unifonic.com>
2024-03-23 01:50:47 +0200

    change arch & add services

--- saas-pod/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/filters/PodRouterFilter.java
+++ saas-pod/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/filters/PodRouterFilter.java
@@ -11,7 +11,6 @@ import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
@@ -31,9 +30,15 @@ public class PodRouterFilter implements GlobalFilter, Ordered {
     }
 
     @SneakyThrows
-    private static URI buildUri(UriComponents uriComponents, PodReferenceDto dto) {
-        log.info("will build uri for {} , {}",dto.reference(),dto.location());
-        return new URI(dto.location() + uriComponents.getPath() + "?" + uriComponents.getQuery());
+    private static URI buildUri(UriComponentsBuilder uriComponents, PodReferenceDto dto) {
+        uriComponents.queryParam("storeId", dto.reference());
+        URI location = new URI(dto.location());
+        uriComponents.host(location.getHost());
+        uriComponents.scheme(location.getScheme());
+        uriComponents.port(location.getPort());
+        URI uri = uriComponents.build().toUri();
+        log.info("will build uri for {} , {} to {}", dto.reference(), dto.location(), uri);
+        return uri;
     }
 
     @Override
@@ -41,8 +46,7 @@ public class PodRouterFilter implements GlobalFilter, Ordered {
         URI uri = exchange.getRequest().getURI();
         String hostName = Optional.ofNullable(exchange.getRequest().getHeaders().getHost()).map(InetSocketAddress::getHostName).orElse(null);
         if (hostName != null) {
-            UriComponents uriComponents = UriComponentsBuilder.fromUri(uri).build();
-            Mono<ServerHttpRequest> httpRequest = getServerHttpRequest(exchange, uriComponents, new Domain(hostName));
+            Mono<ServerHttpRequest> httpRequest = getServerHttpRequest(exchange, UriComponentsBuilder.fromUri(uri), new Domain(hostName));
             return httpRequest.flatMap(it -> {
                 log.info("will route {} to {}", uri, it.getURI());
                 exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, it.getURI());
@@ -55,7 +59,7 @@ public class PodRouterFilter implements GlobalFilter, Ordered {
     }
 
     @SneakyThrows
-    private Mono<ServerHttpRequest> getServerHttpRequest(ServerWebExchange exchange, UriComponents uriComponents, Domain domain) {
+    private Mono<ServerHttpRequest> getServerHttpRequest(ServerWebExchange exchange, UriComponentsBuilder uriComponents, Domain domain) {
         return this.router.getAllocation(domain)
                 .map(dto -> buildUri(uriComponents, dto))
                 .map(newUri -> exchange.getRequest()
