601b32f1780729bc761eaa5298dee19577170e6c
as-mahmoud <asmahmoud@unifonic.com>
2024-04-12 23:56:41 +0200

    add customer api

---
+++ store-pod/store/store-service/src/main/java/com/asrevo/cvhome/store/controller/v1/customer/CustomerApi.java
@@ -0,0 +1,55 @@
+package com.asrevo.cvhome.store.controller.v1.customer;
+
+
+import com.asrevo.cvhome.store.core.constants.Constants;
+import com.asrevo.cvhome.store.core.entity.customer.CustomerCriteria;
+import com.asrevo.cvhome.store.core.entity.merchant.MerchantStore;
+import com.asrevo.cvhome.store.core.entity.reference.language.Language;
+import com.asrevo.cvhome.store.core.model.customer.ReadableCustomerList;
+import com.asrevo.cvhome.store.service.facade.customer.CustomerFacade;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api/v1")
+@Tag(name = "Customer management resource", description = "Manage customers")
+public class CustomerApi {
+
+	@Autowired
+	private CustomerFacade customerFacade;
+
+
+	/**
+	 * Get all customers
+	 *
+	 * @param count
+	 * @return
+	 * @throws Exception
+	 */
+	@GetMapping("/private/customers")
+	@Parameters({
+			@Parameter(name = "store", schema = @Schema(name = "store", type = "string", defaultValue = Constants.DEFAULT_STORE)),
+			@Parameter(name = "lang", schema = @Schema(name = "lang", type = "string", defaultValue = Constants.DEFAULT_LANGUAGE))
+	})
+	public ReadableCustomerList list(@RequestParam(value = "page", required = false) Integer page,
+									 @RequestParam(value = "count", required = false) Integer count,
+									 @Parameter(hidden = true) MerchantStore merchantStore,
+									 @Parameter(hidden = true) Language language) {
+		CustomerCriteria customerCriteria = createCustomerCriteria(page, count);
+		return customerFacade.getListByStore(merchantStore, customerCriteria, language);
+	}
+
+	private CustomerCriteria createCustomerCriteria(Integer start, Integer count) {
+		CustomerCriteria customerCriteria = new CustomerCriteria();
+		Optional.ofNullable(start).ifPresent(customerCriteria::setStartIndex);
+		Optional.ofNullable(count).ifPresent(customerCriteria::setMaxCount);
+		return customerCriteria;
+	}
+
+
+}
