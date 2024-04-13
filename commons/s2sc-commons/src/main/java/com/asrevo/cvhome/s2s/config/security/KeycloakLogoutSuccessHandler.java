5d8301df770d32a452d210a1815fa172d8d61a08
as-mahmoud <asmahmoud@unifonic.com>
2024-04-13 21:13:39 +0200

    fix direct logout

--- commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/config/security/KeycloakLogoutSuccessHandler.java
+++ commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/config/security/KeycloakLogoutSuccessHandler.java
@@ -5,17 +5,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.web.server.DefaultServerRedirectStrategy;
 import org.springframework.security.web.server.ServerRedirectStrategy;
-import org.springframework.security.web.server.WebFilterExchange;
-import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
 import java.util.Optional;
 
-public class KeycloakLogoutSuccessHandler  {
+public class KeycloakLogoutSuccessHandler {
     private final ReactiveClientRegistrationRepository registrationRepository;
     private final ServerRedirectStrategy redirectStrategy = new DefaultServerRedirectStrategy();
     public static final String DEFAULT_LOGOUT_SUCCESS_URL = "/login?logout";
@@ -29,22 +28,36 @@ public class KeycloakLogoutSuccessHandler  {
 
     public Mono<Void> onLogoutSuccess(ServerWebExchange exchange, Authentication authentication) {
         try {
-            OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
-            ClientRegistration clientRegistration = registrationRepository.findByRegistrationId(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId()).block();
-            String logoutUrl = extractLogoutUrl(exchange, clientRegistration);
-            URI logoutSuccessUrl = URI.create(logoutUrl);
-            return this.redirectStrategy.sendRedirect(exchange, logoutSuccessUrl);
-        } catch (Exception e) {
-            return this.redirectStrategy.sendRedirect(exchange, logoutSuccessUrl);
+            if (authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+                ClientRegistration clientRegistration = registrationRepository.findByRegistrationId(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId()).block();
+                if (clientRegistration != null && clientRegistration.getProviderDetails() != null) {
+                    String idToken = null;
+                    if (oAuth2AuthenticationToken.getPrincipal() instanceof DefaultOidcUser defaultOidcUser) {
+                        idToken = defaultOidcUser.getIdToken().getTokenValue();
+                    }
+                    String logoutUrl = extractLogoutUrl(exchange, clientRegistration.getProviderDetails(), idToken);
+                    URI logoutSuccessUrl = URI.create(logoutUrl);
+                    return this.redirectStrategy.sendRedirect(exchange, logoutSuccessUrl);
+                }
+
+            }
+
+        } catch (Exception ignored) {
         }
+        return this.redirectStrategy.sendRedirect(exchange, logoutSuccessUrl);
 
     }
 
-    private static String extractLogoutUrl(ServerWebExchange exchange, ClientRegistration clientRegistration) {
-        ClientRegistration.ProviderDetails providerDetails = clientRegistration.getProviderDetails();
+    private static String extractLogoutUrl(ServerWebExchange exchange, ClientRegistration.ProviderDetails providerDetails, String idToken) {
         ServerHttpRequest request = exchange.getRequest();
         String redirectPath = Optional.ofNullable(exchange.getRequest().getQueryParams().getFirst("redirect_path")).orElse("");
-        String redirectUri = request.getURI().getScheme() + "://" + request.getURI().getAuthority()+"/"+redirectPath;
-        return providerDetails.getAuthorizationUri().replace("protocol/openid-connect/auth", "protocol/openid-connect/logout?post_logout_redirect_uri=" + redirectUri + "&client_id=" + clientRegistration.getClientId());
+        String idTokenHintParam = Optional.ofNullable(idToken)
+                .map(it -> "&id_token_hint=" + it)
+                .orElse("");
+        String authorizationUri = providerDetails.getAuthorizationUri();
+        String redirectUri = request.getURI().getScheme() + "://" + request.getURI().getAuthority() + "/" + redirectPath;
+        String logoutPath = "protocol/openid-connect/logout?post_logout_redirect_uri=" + redirectUri + idTokenHintParam;
+        String logoutUrl = authorizationUri.replace("protocol/openid-connect/auth", logoutPath);
+        return logoutUrl;
     }
 }
