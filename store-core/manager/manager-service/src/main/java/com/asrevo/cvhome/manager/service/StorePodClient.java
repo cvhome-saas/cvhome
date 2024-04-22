fbf02df5b0c512470178179bf8a051e3c728eeec
as-mahmoud <asmahmoud@unifonic.com>
2024-04-23 00:39:34 +0200

    add store management

--- store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/service/StorePodClient.java
+++ store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/service/StorePodClient.java
@@ -1,6 +1,5 @@
 package com.asrevo.cvhome.manager.service;
 
-import com.asrevo.cvhome.storepod.commons.dto.CreateStoreResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
