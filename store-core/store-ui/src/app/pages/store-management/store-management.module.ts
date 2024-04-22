750795c4cbcc675a883de28041cc607488221e13
as-mahmoud <asmahmoud@unifonic.com>
2024-04-22 11:37:04 +0200

    add store management

---
+++ store-core/store-ui/src/app/pages/store-management/store-management.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+
+import { StoreManagementComponent } from './store-management.component';
+import { StoreManagementRoutingModule } from './store-management-routing.module';
+import { StoreDetailsComponent } from './store-details/store-details.component';
+import { StoreCreationComponent } from './store-creation/store-creation.component';
+import { StoresListComponent } from './stores-list/stores-list.component';
+import { StoreFormComponent } from './store-form/store-form.component';
+import { StoreLandingPageComponent } from './store-landing-page/store-landing-page.component';
+import { NgxSummernoteModule } from 'ngx-summernote';
+import { StoreDetailInfoComponent } from './store-detail-info/store-detail-info.component';
+import { StoreBrandingComponent } from './store-branding/store-branding.component';
+import { RetailerComponent } from './retailer/retailer.component';
+import { RetailerListComponent } from './retailer-list/retailer-list.component';
+import { RetailerStoresComponent } from './retailer-stores/retailer-stores.component';
+import { NbDialogModule } from '@nebular/theme';
+import { SharedModule } from "../store-manager/shared/shared.module";
+
+@NgModule({
+  declarations: [
+    StoreManagementComponent,
+    StoreDetailsComponent,
+    StoreCreationComponent,
+    StoresListComponent,
+    StoreFormComponent,
+    StoreBrandingComponent,
+    StoreFormComponent,
+    StoreLandingPageComponent,
+    StoreDetailInfoComponent,
+    RetailerComponent,
+    RetailerListComponent,
+    RetailerStoresComponent
+  ],
+  imports: [
+    StoreManagementRoutingModule,
+
+    /**
+     * stop using AGM, incompatible with Angular 11
+     * look at @types/googlemaps@
+     */
+    /**
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleApiKey,
+      libraries: ['places'],
+      language: 'en'
+    }),
+    **/
+
+    SharedModule,
+    NbDialogModule.forChild(),
+    NgxSummernoteModule
+  ]
+})
+export class StoreManagementModule {
+}
