421aee4440abf3d0c99494c45d7bbaea10673d14
as-mahmoud <asmahmoud@unifonic.com>
2024-04-25 03:39:21 +0200

    add nextjs

--- store-pod/landing-ui/src/app/team/layout.tsx
+++ store-pod/landing-ui/src/app/team/layout.tsx
@@ -1,9 +1,9 @@
 import {Metadata} from "next";
 
 export const metadata: Metadata = {
-    title:"nothing"
+    title: "nothing"
 }
-export default function Layout(p:any) {
+export default function Layout(p: any) {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             welcome
