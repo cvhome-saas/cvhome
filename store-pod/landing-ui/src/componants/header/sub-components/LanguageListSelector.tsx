73e1190f788188e9381ae9dba41d8b6f13e61948
as-mahmoud <asmahmoud@unifonic.com>
2024-04-28 09:44:27 +0300

    add nextjs for landing-ui

--- store-pod/landing-ui/src/componants/header/sub-components/LanguageListSelector.tsx
+++ store-pod/landing-ui/src/componants/header/sub-components/LanguageListSelector.tsx
@@ -1,17 +1,11 @@
 'use client'
-import {useRouter} from "next/navigation";
-import {Language, NEXT_LOCALE_KEY} from "@/componants/header/HeaderTop";
-import Cookies from "js-cookie";
+import {Language} from "@/componants/header/HeaderTop";
+import {usePathname, useRouter} from '@/navigation';
 
 export const LanguageListSelector = ({languages}: { languages: Language[] }) => {
     const router = useRouter();
-
-    function switchLanguage(it: Language) {
-        document.cookie = `${NEXT_LOCALE_KEY}=${it.lang}; path=/; max-age=31536000; SameSite=Lax`;
-        Cookies.set(NEXT_LOCALE_KEY, it.lang)
-        router.refresh();
-        router.push('/' + it.lang);
-    }
+    const pathname = usePathname();
+    const switchLanguage = (it: Language) => router.push(pathname, {locale: it.lang});
 
     return <ul>
         {
