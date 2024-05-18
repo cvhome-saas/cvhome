03f919d1314c1574bd5e9d33d451e0c6907aada2
ashraf <ashraf1abdelrasool@gmail.com>
2024-05-18 12:53:34 +0300

    Wa8 (#15)
    21

--- commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/model/CdnStorageProperties.java
+++ commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/model/CdnStorageProperties.java
@@ -3,5 +3,7 @@ package com.asrevo.cvhome.s2s.model;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("com.asrevo.cvhome.cdn.storage")
-public record CdnStorageProperties(String bucket, String provider) {
+public record CdnStorageProperties(String bucket, StorageProviderType provider, String s3Url, String s3AccessKey,
+                                   String s3SecretKey, String region) {
 }
+
---
+++ commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/model/StorageProviderType.java
@@ -0,0 +1,5 @@
+package com.asrevo.cvhome.s2s.model;
+
+public enum StorageProviderType {
+    S3, MINIO
+}
