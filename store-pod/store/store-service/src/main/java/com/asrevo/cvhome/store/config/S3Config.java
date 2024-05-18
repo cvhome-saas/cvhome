03e9fd1e7e4297dcddbd1abe1ce152db5ccee9e3
as-mahmoud <asmahmoud@unifonic.com>
2024-05-18 09:52:16 +0300

    21

--- store-pod/store/store-service/src/main/java/com/asrevo/cvhome/store/config/S3Config.java
+++ store-pod/store/store-service/src/main/java/com/asrevo/cvhome/store/config/S3Config.java
@@ -1,19 +1,44 @@
 package com.asrevo.cvhome.store.config;
 
 import com.asrevo.cvhome.s2s.model.CdnStorageProperties;
+import com.asrevo.cvhome.s2s.model.StorageProviderType;
 import com.asrevo.cvhome.store.core.modules.cms.content.ContentAssetsManager;
 import com.asrevo.cvhome.store.core.modules.cms.s3.S3StaticContentAssetsManagerImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
+@Slf4j
 public class S3Config {
     @Bean
     @ConditionalOnMissingBean
-    public S3Client s3Client() {
-        return S3Client.create();
+    public S3Client s3Client(CdnStorageProperties properties) throws URISyntaxException {
+        if (StorageProviderType.MINIO.equals(properties.provider())) {
+            return S3Client.builder().endpointOverride(new URI(properties.s3Url()))
+                    .serviceConfiguration(e ->
+                            e
+                                    .pathStyleAccessEnabled(true)
+                                    .chunkedEncodingEnabled(false)
+                    )
+                    .region(Region.of(properties.region()))
+                    .credentialsProvider(StaticCredentialsProvider
+                            .create(AwsBasicCredentials
+                                    .create(properties.s3AccessKey(), properties.s3SecretKey())))
+                    .build();
+
+        } else {
+            return S3Client.create();
+        }
+
     }
 
     @Bean
--- store-pod/store/store-service/src/test/java/com/asrevo/cvhome/store/config/MinioS3Config.java
+++ store-pod/store/store-service/src/test/java/com/asrevo/cvhome/store/config/MinioS3Config.java
@@ -1,16 +1,11 @@
 package com.asrevo.cvhome.store.config;
 
+import com.asrevo.cvhome.s2s.model.StorageProviderType;
 import com.asrevo.cvhome.store.MinIOContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.DynamicPropertyRegistry;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.UUID;
 import java.util.function.Supplier;
 
@@ -25,23 +20,12 @@ public class MinioS3Config {
         Supplier<Object> getUiURL = () -> container.getUiURL() + "/" + bucket;
         properties.add("com.asrevo.cvhome.cdn.basePath", getUiURL);
         properties.add("com.asrevo.cvhome.cdn.storage.bucket", () -> bucket);
-        properties.add("com.asrevo.cvhome.cdn.storage.provider", () -> "minio");
+        properties.add("com.asrevo.cvhome.cdn.storage.provider", () -> StorageProviderType.MINIO);
+        properties.add("com.asrevo.cvhome.cdn.storage.region", () -> "eu-central-1");
+        properties.add("com.asrevo.cvhome.cdn.storage.s3-url", container::getS3URL);
+        properties.add("com.asrevo.cvhome.cdn.storage.s3-access-key", container::getAccessKey);
+        properties.add("com.asrevo.cvhome.cdn.storage.s3-secret-key", container::getSecretKey);
         return container;
-    }
-
-    @Bean
-    public S3Client s3Client(MinIOContainer minIOContainer) throws URISyntaxException {
-        return S3Client.builder().endpointOverride(new URI(minIOContainer.getS3URL()))
-                .serviceConfiguration(e ->
-                        e
-                                .pathStyleAccessEnabled(true)
-                                .chunkedEncodingEnabled(false)
-                )
-                .region(Region.EU_CENTRAL_1)
-                .credentialsProvider(StaticCredentialsProvider
-                        .create(AwsBasicCredentials
-                                .create(minIOContainer.getAccessKey(), minIOContainer.getSecretKey())))
-                .build();
     }
 
 }
