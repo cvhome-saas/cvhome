44d0c2edbb59c2ec665cbfb90cfcd6596c290bfc
as-mahmoud <asmahmoud@unifonic.com>
2024-05-24 15:26:06 +0300

    add default org store structure

--- store-core/manager/manager-service/src/main/resources/data.sql
+++ store-core/manager/manager-service/src/main/resources/data.sql
@@ -1,29 +1,24 @@
-insert into manager.manager_store(id, name, created_date, country, email, phone, owner_id, synced_in_router, synced_in_store)
-VALUES ('65f023632bc46470c104b76f', 'default', current_date, 'EG', 'mail@example.com','+201120266849',
-        'd1952c95-312e-4bb9-9a2d-b703d031276f', true, true)
-on conflict do nothing;
+INSERT INTO manager.manager_store(id, name, created_date, country, email, phone, owner_id, synced_in_router, synced_in_store)
+VALUES ('65f023632bc46470c104b76f', 'default', CURRENT_DATE, 'EG', 'mail@example.com', '+201120266849', 'd1952c95-312e-4bb9-9a2d-b703d031276f', TRUE, TRUE) ON CONFLICT DO NOTHING;
+
+INSERT INTO manager.manager_store(id, name, created_date, country, email, phone, owner_id, synced_in_router, synced_in_store)
+VALUES ('65f023632bc46470c104b75f', 'default', CURRENT_DATE, 'EG', 'mail@example.com', '+201120266849', 'd1952c95-312e-4bb9-9a2d-b703d031276f', TRUE, TRUE) ON CONFLICT DO NOTHING;
+
+INSERT INTO manager.manager_store(id, name, created_date, country, email, phone, owner_id, synced_in_router, synced_in_store)
+VALUES ('65f020632bc46470c104b76f', 'default', CURRENT_DATE, 'EG', 'mail@example.com', '+201120266849', 'd1952c95-312e-4bb6-9a2d-b703d031276f', TRUE, TRUE) ON CONFLICT DO NOTHING;
+
+INSERT INTO manager.manager_store(id, name, created_date, country, email, phone, owner_id, synced_in_router, synced_in_store)
+VALUES ('65f023632bc26470c104b75f', 'default', CURRENT_DATE, 'EG', 'mail@example.com', '+201120266849', 'd1952c95-312e-4bb6-9a2d-b703d031276f', TRUE, TRUE) ON CONFLICT DO NOTHING;
 
 
-insert into manager.reference_alis(id, domain, reference_id)
-values ('65f023632bc46470c204b75f', 'ashraf.com', '65f023632bc46470c104b76f')
-on conflict do nothing;
+INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
+VALUES ('65f023632bc46470c204b75f', 'org1-store1.gateway.com', '65f023632bc46470c104b76f') ON CONFLICT DO NOTHING;
 
-insert into manager.reference_alis(id, domain, reference_id)
-values ('65f023632bc46470c204b25f', 'ali.com', '65f023632bc46470c104b76f')
-on conflict do nothing;
+INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
+VALUES ('65f023632bc46470c204b25f', 'org1-store2.gateway.com', '65f023632bc46470c104b75f') ON CONFLICT DO NOTHING;
 
-insert into manager.reference_alis(id, domain, reference_id)
-values ('65f023632bc16470c204b25f', 'ashraf.onecvhome.click', '65f023632bc46470c104b76f')
-on conflict do nothing;
+INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
+VALUES ('65f023632bc16470c204b25f', 'org2-store1.gateway.com', '65f020632bc46470c104b76f') ON CONFLICT DO NOTHING;
 
-insert into manager.reference_alis(id, domain, reference_id)
-values ('65f023632bc16410c204b25f', 'localhost', '65f023632bc46470c104b76f')
-on conflict do nothing;
-
-insert into manager.reference_alis(id, domain, reference_id)
-values ('65f023632b416410c204b25f', '127.0.0.1', '65f023632bc46470c104b76f')
-on conflict do nothing;
-
-insert into manager.reference_alis(id, domain, reference_id)
-values ('65f023632b416410c203b25f', 'me.asrevo.com', '65f023632bc46470c104b76f')
-on conflict do nothing;
+INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
+VALUES ('65f023632bc16410c204b25f', 'org2-store1.gateway.com', '65f023632bc26470c104b75f') ON CONFLICT DO NOTHING;
