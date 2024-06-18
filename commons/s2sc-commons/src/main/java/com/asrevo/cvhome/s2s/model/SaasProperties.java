6b6b38859d8f616583d29b4811ac3a259beda76d
as-mahmoud <asmahmoud@unifonic.com>
2024-06-18 13:20:55 +0300

    fix native

--- commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/model/SaasProperties.java
+++ commons/s2sc-commons/src/main/java/com/asrevo/cvhome/s2s/model/SaasProperties.java
@@ -1,11 +1,7 @@
 package com.asrevo.cvhome.s2s.model;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Getter
-@Setter
 @ConfigurationProperties("com.asrevo.cvhome.saas")
 public record SaasProperties(String domain) {
 }
