2eb7e3dc38808edb3337b71a4d59d9e4fa3ee9bf
as-mahmoud <asmahmoud@unifonic.com>
2024-06-20 16:11:13 +0300

    fix forward headers

--- store-core/manager/manager-service/src/main/resources/data.sql
+++ store-core/manager/manager-service/src/main/resources/data.sql
@@ -12,13 +12,13 @@ VALUES ('65f023632bc26470c104b75f', 'ORG2-STORE2', CURRENT_DATE, 'EG', 'mail@exa
 
 
 INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
-VALUES ('65f023632bc46470c204b75f', 'org1-store1.gateway.com', '65f023632bc46470c104b76f') ON CONFLICT DO NOTHING;
+VALUES ('65f023632bc46470c204b75f', 'org1-store1.asrevo.com', '65f023632bc46470c104b76f') ON CONFLICT DO NOTHING;
 
 INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
-VALUES ('65f023632bc46470c204b25f', 'org1-store2.gateway.com', '65f023632bc46470c104b75f') ON CONFLICT DO NOTHING;
+VALUES ('65f023632bc46470c204b25f', 'org1-store2.asrevo.com', '65f023632bc46470c104b75f') ON CONFLICT DO NOTHING;
 
 INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
-VALUES ('65f023632bc16470c204b25f', 'org2-store1.gateway.com', '65f020632bc46470c104b76f') ON CONFLICT DO NOTHING;
+VALUES ('65f023632bc16470c204b25f', 'org2-store1.asrevo.com', '65f020632bc46470c104b76f') ON CONFLICT DO NOTHING;
 
 INSERT INTO manager.reference_alis(id, DOMAIN, reference_id)
-VALUES ('65f023632bc16410c204b25f', 'org2-store2.gateway.com', '65f023632bc26470c104b75f') ON CONFLICT DO NOTHING;
+VALUES ('65f023632bc16410c204b25f', 'org2-store2.asrevo.com', '65f023632bc26470c104b75f') ON CONFLICT DO NOTHING;
