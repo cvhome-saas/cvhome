ec4f2b25474472df012f9dfe524860fe7833aa04
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-05-13 16:09:08 +0300

    remove duplicated org classes

--- store-commons/commons/src/main/java/com/asrevo/cvhome/commons/domain/ManagerOrgIdDeserializer.java
+++ store-commons/commons/src/main/java/com/asrevo/cvhome/commons/domain/ManagerOrgIdDeserializer.java
@@ -3,6 +3,7 @@ package com.asrevo.cvhome.commons.domain;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+
 import java.io.IOException;
 
 public class ManagerOrgIdDeserializer extends JsonDeserializer<ManagerOrgId> {
@@ -10,6 +11,10 @@ public class ManagerOrgIdDeserializer extends JsonDeserializer<ManagerOrgId> {
     @Override
     public ManagerOrgId deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         String id = p.getValueAsString();
-        return new ManagerOrgId(id);
+        try {
+            return new ManagerOrgId(id);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
