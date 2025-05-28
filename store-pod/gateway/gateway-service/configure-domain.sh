4813e63ab79b4b7c3ff3a0aa86e45d3689c7adb0
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-05-28 17:45:57 +0300

    fix rename auth

--- store-pod/gateway/gateway-service/configure-domain.sh
+++ store-pod/gateway/gateway-service/configure-domain.sh
@@ -1,7 +1,7 @@
 echo '127.0.0.1	gateway.com' | sudo tee -a /etc/hosts > /dev/null
 echo '127.0.0.1	www.gateway.com' | sudo tee -a /etc/hosts > /dev/null
 echo '127.0.0.1	admin.gateway.com' | sudo tee -a /etc/hosts > /dev/null
-echo '127.0.0.1	auth.gateway.com' | sudo tee -a /etc/hosts > /dev/null
+echo '127.0.0.1	core-auth.gateway.com' | sudo tee -a /etc/hosts > /dev/null
 echo '127.0.0.1	ashraf.gateway.com' | sudo tee -a /etc/hosts > /dev/null
 echo '127.0.0.1	ashraf.com' | sudo tee -a /etc/hosts > /dev/null
 echo '127.0.0.1	dcm' | sudo tee -a /etc/hosts > /dev/null
