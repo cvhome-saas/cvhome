3fe3bdacb51e90e5d46d8165c04ffdeda2e7bcc3
aabdelhafiz.c <ashraf1abdelrasool@gmail.com>
2025-09-20 00:18:01 +0300

    1

--- store-core/manager/manager-service/src/test/java/com/asrevo/cvhome/manager/TestcontainersConfiguration.java
+++ store-core/manager/manager-service/src/test/java/com/asrevo/cvhome/manager/TestcontainersConfiguration.java
@@ -1,8 +1,14 @@
 package com.asrevo.cvhome.manager;
 
+import com.asrevo.cvhome.commons.domain.ManagerOrgId;
 import com.asrevo.cvhome.s2s.config.ReactiveTestCustomSecurityConfig;
+import com.asrevo.cvhome.subscription.api.SubscriptionPlanDetailsService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClient;
+import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryProperties;
+import org.springframework.cloud.client.discovery.simple.reactive.SimpleReactiveDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -22,5 +28,17 @@ public class TestcontainersConfiguration {
     @ServiceConnection
     RabbitMQContainer rabbitContainer() {
         return new RabbitMQContainer(DockerImageName.parse("rabbitmq:4.0.2-management"));
+    }
+
+    @Bean
+    public CommandLineRunner runner(SubscriptionPlanDetailsService subscriptionPlanDetailsService, SimpleDiscoveryProperties properties, SimpleReactiveDiscoveryClient simpleDiscoveryClient) {
+        return args -> {
+            simpleDiscoveryClient.getServices().subscribe(System.out::println);
+            
+            System.out.println(properties.getInstances().size());
+            subscriptionPlanDetailsService.subscriptionPlanDetails(new ManagerOrgId("21f023932bc66470c104b76f")).subscribe(it -> {
+                System.out.println(it);
+            });
+        };
     }
 }
