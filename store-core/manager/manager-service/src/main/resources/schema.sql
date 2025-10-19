b8d868eb0a6ca72f816bc158b9b9bfdf93fe349a
aabdelhafiz.c <aabdelhafiz.c@tahakom.com>
2025-10-19 15:23:16 +0300

    Introduce `ManagerStorePreferences` entity and associated converters for managing store preferences. Add `preferences` column to `manager_store` schema. Update service and controller logic to handle preferences.
    add test route
    
    Move `ManagerStorePreferences` to `commons.domain` package and update references for better modularization.
    
    Add ManagerStorePreferences entity and preferences column

--- store-core/manager/manager-service/src/main/resources/schema.sql
+++ store-core/manager/manager-service/src/main/resources/schema.sql
@@ -9,12 +9,13 @@ CREATE TABLE IF NOT EXISTS manager.manager_org
 );
 CREATE TABLE IF NOT EXISTS manager.manager_store
 (
-    id                 varchar(24) not null,
-    name               varchar(50) not null,
-    created_date       timestamp   not null,
-    org_id             varchar(24) not null,
-    pod_id             varchar(24) not null,
-    provisioning_state varchar(30) not null,
+    id                 varchar(24)   not null,
+    name               varchar(50)   not null,
+    created_date       timestamp     not null,
+    org_id             varchar(24)   not null,
+    pod_id             varchar(24)   not null,
+    provisioning_state varchar(30)   not null,
+    preferences        varchar(1024) not null,
     version            int,
     constraint manager_store_pk primary key (id),
     constraint manager_store_manager_fk foreign key (org_id) references manager.manager_org (id)
