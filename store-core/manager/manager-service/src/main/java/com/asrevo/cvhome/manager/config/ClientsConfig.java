82ba170efb755dc96a6c33eb1c0bf6f90c45d081
Ashraf Mahmoud <ashraf1abdelrasool@gmail.com>
2025-10-20 13:13:00 +0300

    remove unused clients

--- store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/config/ClientsConfig.java
+++ store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/config/ClientsConfig.java
@@ -1,7 +1,5 @@
 package com.asrevo.cvhome.manager.config;
 
-import com.asrevo.cvhome.merchant.api.ExternalReactiveMerchantStoreService;
-import com.asrevo.cvhome.merchant.api.StorePodClient;
 import com.asrevo.cvhome.s2s.config.internal.WebClientBuilder;
 import com.asrevo.cvhome.subscription.api.SubscriptionHttpEventsService;
 import com.asrevo.cvhome.subscription.api.SubscriptionPlanDetailsService;
@@ -17,18 +15,8 @@ import org.springframework.context.annotation.Configuration;
 public class ClientsConfig {
 
 	@Bean
-	public StorePodClient storePodClient(WebClientBuilder webClientBuilder) {
-		return webClientBuilder.buildClient("merchant", StorePodClient.class);
-	}
-
-	@Bean
 	public SubscriptionPlanDetailsService subscriptionPlanDetailsService(WebClientBuilder webClientBuilder) {
 		return webClientBuilder.buildClient("subscription", SubscriptionPlanDetailsService.class);
-	}
-
-	@Bean
-	public ExternalReactiveMerchantStoreService externalMerchantStoreService(WebClientBuilder webClientBuilder) {
-		return webClientBuilder.buildClient("merchant", ExternalReactiveMerchantStoreService.class);
 	}
 
 	@Bean
