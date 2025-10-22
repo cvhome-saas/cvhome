9ebc8cd8e804254f565ca101fca3e2532fb63cce
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-10-22 21:24:39 +0300

    add PodProxyController and update ProxyClient for flexible request forwarding

--- store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/service/ProxyClient.java
+++ store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/service/ProxyClient.java
@@ -8,16 +8,19 @@ import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public record ProxyClient(WebClient storeProxyClient) {
 
-	public <T> Mono<ResponseEntity<T>> forward(HttpMethod method, String uri,
+	public <T> Mono<ResponseEntity<T>> forward(HttpMethod method, String uri, Map<String, String> headers,
 			Supplier<BodyInserter<?, ? super ClientHttpRequest>> bodyExtractor,
-			Function<ClientResponse, Mono<ResponseEntity<T>>> bodyyy) {
+			Function<ClientResponse, Mono<ResponseEntity<T>>> bodyMapper) {
+
 		WebClient.RequestBodySpec requestSpec = storeProxyClient.method(method).uri(uri);
-		return requestSpec.body(bodyExtractor.get()).exchangeToMono(bodyyy);
+		requestSpec.headers(httpHeaders -> headers.forEach(httpHeaders::add));
+		return requestSpec.body(bodyExtractor.get()).exchangeToMono(bodyMapper);
 	}
 
 }
