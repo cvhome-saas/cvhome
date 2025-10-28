dfad7f3eee48f0c2c207c4ae6e9d3a07ddd0537a
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-10-28 23:53:18 +0300

    remove gateway

--- store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/utils/Defines.java
+++ store-core/manager/manager-service/src/main/java/com/asrevo/cvhome/manager/utils/Defines.java
@@ -4,6 +4,6 @@ public class Defines {
 
 	public static final String SAAS_POD_SUFFIX = "store-pod-saas-gateway";
 
-	public static final String STORE_POD_PREFIX = "store-pod-gateway";
+	public static final String STORE_POD_PREFIX = "store-pod";
 
 }
