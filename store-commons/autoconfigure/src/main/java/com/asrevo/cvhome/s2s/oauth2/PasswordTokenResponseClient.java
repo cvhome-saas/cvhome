07ff659dadeb0c5d9e63f6324cbc69625603ba82
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-11-07 00:08:00 +0300

    switch ServerCallBearerExchangeInterceptor to client_credentials grant type and clean up associated configurations

--- store-commons/autoconfigure/src/main/java/com/asrevo/cvhome/s2s/oauth2/PasswordTokenResponseClient.java
+++ store-commons/autoconfigure/src/main/java/com/asrevo/cvhome/s2s/oauth2/PasswordTokenResponseClient.java
@@ -3,22 +3,20 @@ package com.asrevo.cvhome.s2s.oauth2;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.client.endpoint.OAuth2PasswordGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequest;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-public class PasswordTokenResponseClient extends OAuth2AccessTokenResponseClient<OAuth2PasswordGrantRequest> {
+public class PasswordTokenResponseClient extends OAuth2AccessTokenResponseClient<OAuth2ClientCredentialsGrantRequest> {
 
 	@Override
-	HttpEntity<?> getRequestEntity(OAuth2PasswordGrantRequest request) {
+	HttpEntity<?> getRequestEntity(OAuth2ClientCredentialsGrantRequest request) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
 		map.add("client_id", request.getClientRegistration().getClientId());
 		map.add("client_secret", request.getClientRegistration().getClientSecret());
 		map.add("grant_type", request.getGrantType().getValue());
-		map.add("username", request.getUsername());
-		map.add("password", request.getPassword());
 		return new HttpEntity<>(map, headers);
 	}
 
