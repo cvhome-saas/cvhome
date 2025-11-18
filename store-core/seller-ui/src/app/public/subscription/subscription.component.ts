381ecb0d39a03d86a2968a5a3930cfd4bd539b2d
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-11-19 02:44:21 +0300

    FIX MISSING CSS

--- store-core/seller-ui/src/app/public/subscription/subscription.component.ts
+++ store-core/seller-ui/src/app/public/subscription/subscription.component.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'ngx-subscription',
     standalone: false,
-    styleUrls: ['subscription.component.scss'],
     template: `
         <router-outlet></router-outlet>
     `,
