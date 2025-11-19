7763481286497f2e0e9ed4ef082854351e975255
Ashraf Mahmoud <ashraf1abdelrasool@gmail.com>
2025-11-19 15:40:01 +0300

    feat

--- store-pod/landing-ui/templates/default/next.config.ts
+++ store-pod/landing-ui/templates/default/next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    output: 'standalone',
     images: {
         unoptimized: true
     },
