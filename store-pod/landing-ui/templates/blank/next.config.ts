ec7dc23b9624b9bcdc7ecc2c2c10560f75acca84
Ashraf Mahmoud <ashraf1abdelrasool@gmail.com>
2025-11-20 10:00:59 +0300

    minimal docker image

--- store-pod/landing-ui/templates/blank/next.config.ts
+++ store-pod/landing-ui/templates/blank/next.config.ts
@@ -3,7 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
     images: {
         unoptimized: true
     },
