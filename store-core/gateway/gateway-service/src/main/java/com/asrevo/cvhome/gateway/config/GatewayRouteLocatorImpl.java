4f4101f488a93420b418fe5906598a1f7c32656b
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-11-22 23:02:12 +0300

    use pod as router param from gateway to pod

--- store-core/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/config/GatewayRouteLocatorImpl.java
+++ store-core/gateway/gateway-service/src/main/java/com/asrevo/cvhome/gateway/config/GatewayRouteLocatorImpl.java
@@ -1,16 +1,11 @@
 package com.asrevo.cvhome.gateway.config;
 
-import com.asrevo.cvhome.commons.domain.ManagerStoreId;
 import com.asrevo.cvhome.commons.domain.Pod;
-import com.asrevo.cvhome.controlplane.manager.api.RouterAllocationService;
+import com.asrevo.cvhome.commons.domain.PodId;
 import com.asrevo.cvhome.s2s.config.gateway.FHostRoutePredicateFactory;
 import com.asrevo.cvhome.s2s.config.gateway.FNotServiceRoutePredicateFactory;
 import com.asrevo.cvhome.s2s.config.internal.ServiceUrlBuilder;
 import com.asrevo.cvhome.s2s.model.ServiceDomainProperties;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.handler.AsyncPredicate;
 import org.springframework.cloud.gateway.route.Route;
@@ -21,6 +16,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
 
 @Component
 @RequiredArgsConstructor
@@ -33,8 +33,6 @@ public class GatewayRouteLocatorImpl implements RouteLocator {
 	private final FNotServiceRoutePredicateFactory notServicePredicate;
 
 	private final FHostRoutePredicateFactory hostRoutePredicate;
-
-	private final RouterAllocationService router;
 
 	private final Environment environment;
 
@@ -52,63 +50,37 @@ public class GatewayRouteLocatorImpl implements RouteLocator {
 			.route(r -> r.path("/control-plane/**")
 				.filters(f -> f.stripPrefix(1).tokenRelay().preserveHostHeader())
 				.uri("lb://control-plane"))
-			.route(r -> {
-				return r.predicate(notBackendService).and().predicate(wwwHostPredicate).uri("lb://seller-ui");
-			});
+			.route(r -> r.predicate(notBackendService).and().predicate(wwwHostPredicate).uri("lb://seller-ui"));
 
 		List<Pod> allPods = Optional.ofNullable(serviceDomainProperties.pods()).orElse(List.of());
 		for (Pod podNameSpace : allPods) {
-			addPodServiceExactMatchPod(podNameSpace, route, "store-pod-gateway");
+			addPodServiceExactMatchPod(podNameSpace, route);
 		}
 
 		return route.build().getRoutes();
 	}
 
-	private void addPodServiceExactMatchPod(Pod pod, RouteLocatorBuilder.Builder route, String serviceName) {
+	private void addPodServiceExactMatchPod(Pod pod, RouteLocatorBuilder.Builder route) {
 		ServiceUrlBuilder serviceUrlBuilder = new ServiceUrlBuilder(serviceDomainProperties, environment);
-		route.route("store-" + serviceName + "-v1-" + pod.id().id(), r -> {
-			return r.path("/" + serviceName + "/**")
-				.and()
-				.asyncPredicate(checkStoreMatchPodFromParam(pod))
-				.filters(f -> f.stripPrefix(1).tokenRelay())
-				.uri(serviceUrlBuilder.getServiceUrl(pod));
-		});
-		route.route("store-" + serviceName + "-v2-" + pod.id().id(), r -> {
-			return r.path("/" + serviceName + "-v2/**")
-				.and()
-				.asyncPredicate(checkStoreMatchPodFromUrl(pod))
-				.filters(f -> f.stripPrefix(2).tokenRelay())
-				.uri(serviceUrlBuilder.getServiceUrl(pod));
-		});
+		route.route("store-store-pod-gateway-v1-" + pod.id().id(),
+				r -> r.path("/store-pod-gateway/**")
+					.and()
+					.query("store")
+					.and()
+					.query("pod")
+					.and()
+					.asyncPredicate(checkStoreMatchPodFromParam(pod.id()))
+					.filters(f -> f.stripPrefix(1).tokenRelay())
+					.uri(serviceUrlBuilder.getServiceUrl(pod)));
 	}
 
-	private AsyncPredicate<ServerWebExchange> checkStoreMatchPodFromParam(Pod pod) {
-		return serverWebExchange -> {
-			ManagerStoreId store = Optional
-				.ofNullable(serverWebExchange.getRequest().getQueryParams().getFirst("store"))
-				.filter(it -> !it.trim().isEmpty())
-				.map(ManagerStoreId::new)
-				.orElse(null);
-			return isStoreInPod(store, pod);
-		};
-	}
-
-	private AsyncPredicate<ServerWebExchange> checkStoreMatchPodFromUrl(Pod pod) {
-		return serverWebExchange -> {
-			ManagerStoreId store = Optional.ofNullable(serverWebExchange.getRequest().getURI().getPath())
-				.map(url -> new ManagerStoreId(url.split("/")[2]))
-				.orElse(null);
-			return isStoreInPod(store, pod);
-		};
-	}
-
-	private Mono<Boolean> isStoreInPod(ManagerStoreId store, Pod pod) {
-		if (store == null || pod == null) {
-			return Mono.just(Boolean.FALSE);
-		}
-		return this.router.getStorePodByStoreId(store)
-			.map(it -> it.id().equals(pod.id()))
-			.defaultIfEmpty(Boolean.FALSE);
+	private AsyncPredicate<ServerWebExchange> checkStoreMatchPodFromParam(PodId podId) {
+		return serverWebExchange -> Optional.ofNullable(serverWebExchange.getRequest().getQueryParams().getFirst("pod"))
+			.filter(it -> !it.trim().isEmpty())
+			.map(PodId::new)
+			.map(it -> it.equals(podId))
+			.map(Mono::just)
+			.orElseGet(() -> Mono.just(false));
 	}
 
 }
