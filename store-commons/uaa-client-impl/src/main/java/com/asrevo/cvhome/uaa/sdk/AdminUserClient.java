3791d07a32ca9d85ebc90795e4f2e411fabf9209
ashraf <ashraf1abdelrasool@gmail.com>
2026-03-18 16:11:33 +0200

    update UAA build configuration and Gradle Docker build command

--- store-commons/uaa-client-impl/src/main/java/com/asrevo/cvhome/uaa/sdk/AdminUserClient.java
+++ store-commons/uaa-client-impl/src/main/java/com/asrevo/cvhome/uaa/sdk/AdminUserClient.java
@@ -9,7 +9,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.util.UUID;
 
 public class AdminUserClient extends AbstractAdminClient {
 
