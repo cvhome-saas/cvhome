2c0b568733256166aaef6fc9e61f7b515492450c
ashraf <ashraf1abdelrasool@gmail.com>
2026-03-20 14:23:55 +0200

    update UAA build configuration and Gradle Docker build command

--- store-core/uaa/src/main/java/com/asrevo/cvhome/uaa/config/JwtCustomizerConfig.java
+++ store-core/uaa/src/main/java/com/asrevo/cvhome/uaa/config/JwtCustomizerConfig.java
@@ -2,10 +2,12 @@ package com.asrevo.cvhome.uaa.config;
 
 import com.asrevo.cvhome.uaa.domain.Role;
 import com.asrevo.cvhome.uaa.domain.User;
+import com.asrevo.cvhome.uaa.mapper.ClientClientDetailsMapper;
 import com.asrevo.cvhome.uaa.repo.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
@@ -25,26 +27,53 @@ public class JwtCustomizerConfig {
 	OAuth2TokenCustomizer<JwtEncodingContext> oauth2TokenCustomizer() {
 		return context -> {
 			// Only enrich access tokens
-			if (!"access_token".equals(context.getTokenType().getValue())) {
+			if (!OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
 				return;
 			}
 
-			Authentication principal = context.getPrincipal();
-			if (principal == null)
-				return;
+			addClientSettingClaims(context);
 
-			String username = principal.getName();
-			userRepository.findByUsername(username).ifPresent(user -> addUserClaims(context, user));
+			addTokenSettingsClaims(context);
+
+			addUserClaims(context);
 		};
 	}
 
-	private void addUserClaims(JwtEncodingContext context, User user) {
-		// roles
-		Set<String> roles = user.getRoles().stream().map(Role::getName).collect(Collectors.toSet());
-		if (!roles.isEmpty()) {
-			context.getClaims().claim("roles", roles);
-		}
-		user.getMetadata().forEach((s, o) -> context.getClaims().claim(s, o));
+	private static void addTokenSettingsClaims(JwtEncodingContext context) {
+		var tokenSettings = context.getRegisteredClient().getTokenSettings();
+		tokenSettings.getSettings().forEach((s, o) -> {
+			if (!ClientClientDetailsMapper.KNOWN_TOKEN_SETTINGS.contains(s)) {
+				context.getClaims().claim(s, o);
+			}
+		});
+	}
+
+	private static void addClientSettingClaims(JwtEncodingContext context) {
+		var clientSettings = context.getRegisteredClient().getClientSettings();
+		clientSettings.getSettings().forEach((s, o) -> {
+			if (!ClientClientDetailsMapper.KNOWN_CLIENT_SETTINGS.contains(s)) {
+				context.getClaims().claim(s, o);
+			}
+		});
+	}
+
+	private void addUserClaims(JwtEncodingContext context) {
+
+		Authentication principal = context.getPrincipal();
+		if (principal == null)
+			return;
+
+		String username = principal.getName();
+		userRepository.findByUsername(username).ifPresent(user -> {
+			// roles
+			Set<String> roles = user.getRoles().stream().map(Role::getName).collect(Collectors.toSet());
+			if (!roles.isEmpty()) {
+				context.getClaims().claim("roles", roles);
+			}
+			user.getMetadata().forEach((s, o) -> context.getClaims().claim(s, o));
+
+		});
+
 	}
 
 }
--- store-core/uaa/src/main/java/com/asrevo/cvhome/uaa/dto/ClientDetailsTokens.java
+++ store-core/uaa/src/main/java/com/asrevo/cvhome/uaa/dto/ClientDetailsTokens.java
@@ -4,10 +4,11 @@ import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 
 import java.time.Duration;
+import java.util.Map;
 
 public record ClientDetailsTokens(Duration authorizationCodeTimeToLive, Duration accessTokenTimeToLive,
 		OAuth2TokenFormat accessTokenFormat, Duration deviceCodeTimeToLive, boolean reuseRefreshTokens,
 		Duration refreshTokenTimeToLive, SignatureAlgorithm idTokenSignatureAlgorithm,
-		boolean x509CertificateBoundAccessTokens) {
+		boolean x509CertificateBoundAccessTokens, Map<String, Object> customSettings) {
 
 }
