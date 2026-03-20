5706edf1c06bc8c9cb3c640ad87366512022eb91
ashraf <ashraf1abdelrasool@gmail.com>
2026-03-21 01:25:12 +0200

    update UAA build configuration and Gradle Docker build command (#178)

--- store-core/uaa/src/main/java/com/asrevo/cvhome/uaa/dto/ClientDetailsSettings.java
+++ store-core/uaa/src/main/java/com/asrevo/cvhome/uaa/dto/ClientDetailsSettings.java
@@ -1,8 +1,10 @@
 package com.asrevo.cvhome.uaa.dto;
 
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import java.util.Map;
 
 public record ClientDetailsSettings(boolean requireProofKey, boolean requireAuthorizationConsent, String jwkSetUrl,
-		SignatureAlgorithm tokenEndpointAuthenticationSigningAlgorithm, String x509CertificateSubjectDN) {
+		SignatureAlgorithm tokenEndpointAuthenticationSigningAlgorithm, String x509CertificateSubjectDN,
+		Map<String, Object> customSettings) {
 
 }
