dbc12480168896d2d55254771806138a2ca49d1d
ashraf <ashraf1abdelrasool@gmail.com>
2026-03-27 00:23:47 +0200

    Introduce CUA service with OAuth2 authorization support, configuration setup, and initial data seeding.

---
+++ store-pod/cua/src/main/java/com/asrevo/cvhome/cua/exception/ForbiddenOperationException.java
@@ -0,0 +1,9 @@
+package com.asrevo.cvhome.cua.exception;
+
+public class ForbiddenOperationException extends RuntimeException {
+
+	public ForbiddenOperationException(String message) {
+		super(message);
+	}
+
+}
