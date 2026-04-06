63443b16d8cec1a98141334b1ebf726420f0a32c
ashraf <ashraf1abdelrasool@gmail.com>
2026-04-07 01:02:01 +0300

    Introduce Flyway for database migrations and seed initial schema and data for UAA module

--- store-core/uaa/src/main/resources/db/migration/V2__Initial_Data.sql
+++ store-core/uaa/src/main/resources/db/migration/V2__Initial_Data.sql
@@ -3,7 +3,7 @@ insert into uaa.oauth2_registered_client (id, client_id, client_id_issued_at, cl
                                           client_authentication_methods, authorization_grant_types, redirect_uris,
                                           post_logout_redirect_uris, scopes, client_settings, token_settings)
 values ('a5c7e2c0-7e7e-8f5f-c2d2-7e7e8f5fc2d2',
-        'admin-sdk', now(), '{bcrypt}$2a$10$KEyYNPGHPotegD5Ui8/yX.WzIv75INVsEzgjkD2GqQdmcBG0qke8i', 'Admin SDK',
+        'admin-sdk', now(), '${admin_sdk_secret_hash}', 'Admin SDK',
         'client_secret_basic', 'client_credentials,refresh_token',
         NULL, NULL,
         'super_admin',
@@ -16,7 +16,7 @@ insert into uaa.oauth2_registered_client (id, client_id, client_id_issued_at, cl
                                           client_authentication_methods, authorization_grant_types, redirect_uris,
                                           post_logout_redirect_uris, scopes, client_settings, token_settings)
 values ('b6d8f3d1-8f8f-9060-d3e3-8f8f9060d3e3',
-        'web-app', now(), '{bcrypt}$2a$10$KEyYNPGHPotegD5Ui8/yX.WzIv75INVsEzgjkD2GqQdmcBG0qke8i', 'Web App',
+        'web-app', now(), '${web_app_secret_hash}', 'Web App',
         'client_secret_basic', 'authorization_code,refresh_token',
         'http://localhost:8000/login/oauth2/code/uaa,http://gateway.com:8000/login/oauth2/code/uaa,http://seller-ui.gateway.com:8000/login/oauth2/code/uaa,https://asrevo.click/login/oauth2/code/uaa,https://www.asrevo.click/login/oauth2/code/uaa,https://seller-ui.asrevo.click/login/oauth2/code/uaa',
         'http://localhost:8000,http://gateway.com:8000,http://seller-ui.gateway.com:8000,https://asrevo.click,https://www.asrevo.click,https://seller-ui.asrevo.click',
@@ -28,26 +28,26 @@ on conflict (id) do nothing;
 insert into uaa.oauth2_registered_client (id, client_id, client_id_issued_at, client_secret, client_name,
                                           client_authentication_methods, authorization_grant_types, redirect_uris,
                                           post_logout_redirect_uris, scopes, client_settings, token_settings)
-values ('BECF0252-14DD-437A-85B8-0C8EEF1BD03F',
-        'store-pod-507f1f77@service.store-pod.internal', now(),
-        '{bcrypt}$2a$10$KEyYNPGHPotegD5Ui8/yX.WzIv75INVsEzgjkD2GqQdmcBG0qke8i', 'Store Service (Store Pod 1)',
+values ('608A79F2-CB4D-42CA-8BA9-2571DE69BDE8',
+        'store-core@service.store-core.internal', now(),
+        '${store_core_secret_hash}', 'Store Core (Store Core)',
         'client_secret_basic', 'client_credentials',
         NULL, NULL,
-        'internal',
-        '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false,"resource":"pod-507f1f77"}',
+        'internal,store',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
         '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":false,"settings.token.x509-certificate-bound-access-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",86400.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}')
 on conflict (id) do nothing;
 
 insert into uaa.oauth2_registered_client (id, client_id, client_id_issued_at, client_secret, client_name,
                                           client_authentication_methods, authorization_grant_types, redirect_uris,
                                           post_logout_redirect_uris, scopes, client_settings, token_settings)
-values ('608A79F2-CB4D-42CA-8BA9-2571DE69BDE8',
-        'store-core@service.store-core.internal', now(),
-        '{bcrypt}$2a$10$KEyYNPGHPotegD5Ui8/yX.WzIv75INVsEzgjkD2GqQdmcBG0qke8i', 'Store Core (Store Core)',
+values ('BECF0252-14DD-437A-85B8-0C8EEF1BD03F',
+        'store-pod-507f1f77@service.store-pod.internal', now(),
+        '${store_pod_secret_hash}', 'Store Service (Store Pod 1)',
         'client_secret_basic', 'client_credentials',
         NULL, NULL,
-        'internal,store',
-        '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
+        'internal',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false,"resource":"pod-507f1f77"}',
         '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":false,"settings.token.x509-certificate-bound-access-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",86400.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}')
 on conflict (id) do nothing;
 
@@ -63,65 +63,65 @@ on conflict (id) do nothing;
 -- Seed users
 insert into uaa.users (id, username, email, first_name, last_name, password_hash, enabled, metadata)
 values ('65D8419C-8765-4B8B-A15F-910DCE959931', 'super-admin', 'super-admin@mail.com', 'Super', 'Admin',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE, '{}'),
+        '${super_admin_password_hash}', TRUE, '{}'),
        ('318F2FD5-E235-4C2E-AB7E-6C949BA4CDD4', 'org1-admin', 'org1-admin@mail.com', 'Org1', 'Admin',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "21f023932bc66470c104b76f"
         }'),
        ('60AB49A5-7F06-4B5A-BE81-9B30BB6559AE', 'org1-store1-admin', 'org1-store1-admin@mail.com', 'Store1', 'Admin',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "21f023932bc66470c104b76f",
           "store": "65f023632bc46470c104b76f"
         }'),
        ('0C1C7C69-F504-47E2-AA5D-3348CBD1023F', 'org1-store1-moderator', 'org1-store1-moderator@mail.com', 'Store1',
         'Moderator',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "21f023932bc66470c104b76f",
           "store": "65f023632bc46470c104b76f"
         }'),
        ('E91EB99A-D3C9-4FBE-8CD3-8744E4F6CA29', 'org1-store2-admin', 'org1-store2-admin@mail.com', 'Store2', 'Admin',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "21f023932bc66470c104b76f",
           "store": "65f023632bc46470c104b75f"
         }'),
        ('77E97256-9706-44A9-A640-52AF9C65E5EA', 'org1-store2-moderator', 'org1-store2-moderator@mail.com', 'Store2',
         'Moderator',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "21f023932bc66470c104b76f",
           "store": "65f023632bc46470c104b75f"
         }'),
        ('7B54CF3C-5510-40BF-BD0B-14C4078EDF07', 'org2-admin', 'org2-admin@mail.com', 'Org2', 'Admin',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "352023632b046970c104b76f"
         }'),
        ('6C303A6A-459C-44FB-B4B2-EAB53FB2B325', 'org2-store1-admin', 'org2-store1-admin@mail.com', 'Store1', 'Admin',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "352023632b046970c104b76f",
           "store": "65f020632bc46470c104b76f"
         }'),
        ('F900B31E-376E-4757-8EAB-501ABA2CFDD3', 'org2-store1-moderator', 'org2-store1-moderator@mail.com', 'Store1',
         'Moderator',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "352023632b046970c104b76f",
           "store": "65f020632bc46470c104b76f"
         }'),
        ('F1FA1FA2-51F3-41CD-A816-356F6816ABD4', 'org2-store2-admin', 'org2-store2-admin@mail.com', 'Store2', 'Admin',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "352023632b046970c104b76f",
           "store": "65f023632bc26470c104b75f"
         }'),
        ('97022CD5-CC0A-467A-A99A-460B8E2745C3', 'org2-store2-moderator', 'org2-store2-moderator@mail.com', 'Store2',
         'Moderator',
-        '{bcrypt}$2a$10$pse9zsAXkH/zOjZpfiP7X.weD6CNtVY/NR5A4mYUwbYqcYThHchRa', TRUE,
+        '${default_user_password_hash}', TRUE,
         '{
           "org": "352023632b046970c104b76f",
           "store": "65f023632bc26470c104b75f"
