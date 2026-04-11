5b1ca7ef6c5ea6e51734f43b2be9c81ca2473a20
ashraf <ashraf1abdelrasool@gmail.com>
2026-04-11 21:20:47 +0300

    add role level security checks

--- store-commons/commons/src/main/java/com/asrevo/cvhome/commons/annotation/SecuredResource.java
+++ store-commons/commons/src/main/java/com/asrevo/cvhome/commons/annotation/SecuredResource.java
@@ -12,4 +12,22 @@ public @interface SecuredResource {
 
 	Roles[] roles() default {};
 
+	Roles[] CUSTOMER_ROLES = { Roles.ROLE_CUSTOMER };
+
+	Roles[] STORE_ROLES = { Roles.ROLE_STORE_ADMIN, Roles.ROLE_STORE_MODERATOR };
+
+	Roles[] ORG_ROLES = { Roles.ROLE_ORG_ADMIN };
+
+	Roles[] ORG_STORE_ROLES = { Roles.ROLE_ORG_ADMIN, Roles.ROLE_STORE_ADMIN, Roles.ROLE_STORE_MODERATOR };
+
+	Roles[] INTERNAL_ROLES = { Roles.SCOPE_INTERNAL };
+
+	Roles[] STORE_RETAIL_ROLES = { Roles.ROLE_STORE_RETAIL };
+
+	Roles[] STORE_ADMIN_ROLES = { Roles.ROLE_STORE_ADMIN };
+
+	Roles[] STORE_MODERATOR_ROLES = { Roles.ROLE_STORE_MODERATOR };
+
+	Roles[] SUPER_ROLES = { Roles.ROLE_SUPER_ADMIN };
+
 }
