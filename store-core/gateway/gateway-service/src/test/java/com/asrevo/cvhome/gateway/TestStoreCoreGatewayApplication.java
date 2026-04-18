2e702481db47fb9b357c9556ecdccdbccf7af87e
ashraf <ashraf1abdelrasool@gmail.com>
2026-04-18 17:04:14 +0300

    manual imporvments

--- store-core/gateway/gateway-service/src/test/java/com/asrevo/cvhome/gateway/TestStoreCoreGatewayApplication.java
+++ store-core/gateway/gateway-service/src/test/java/com/asrevo/cvhome/gateway/TestStoreCoreGatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 public class TestStoreCoreGatewayApplication {
 
+    private TestStoreCoreGatewayApplication() {
+    }
+
     public static void main(String[] args) {
         SpringApplication.from(StoreCoreGatewayApplication::main).with(TestcontainersConfiguration.class).run(args);
     }
