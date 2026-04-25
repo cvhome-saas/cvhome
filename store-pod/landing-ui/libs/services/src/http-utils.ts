ce9dac4cbf44d28d4f4406cd30c6ec8bc511fb5d
ashraf <ashraf1abdelrasool@gmail.com>
2026-04-25 21:07:31 +0300

    manual imporvments

--- store-pod/landing-ui/libs/services/src/http-utils.ts
+++ store-pod/landing-ui/libs/services/src/http-utils.ts
@@ -2,8 +2,8 @@ export function handleResponse<T>(it: Response): T | undefined {
     if (it.ok) {
         return it.json() as unknown as T;
     } else {
-        console.error(it);
-        throw new Error(it.statusText);
+        console.error(`Request failed: ${it.status} ${it.statusText} (${it.url})`);
+        return undefined;
     }
 }
 
