fefe0a81f63fbd52a683deffb8bb69d6edb44fdf
ashraf <ashraf1abdelrasool@gmail.com>
2026-07-20 21:02:27 +0300

    add config for test stores (#236)
    * add config for test stores
    
    * test sotres sql files
    
    * test sotres sql files
    
    * move super admin config to main sql files
    
    * move super admin config to main sql files
    
    * add super admin config
    
    * add super admin config
    
    * add super admin config
    
    * add super admin config
    
    * add super admin config
    
    * add super admin config
    
    * add super admin config

--- store-commons/autoconfigure/src/main/java/com/asrevo/cvhome/s2s/config/CvhomeSharedConfig.java
+++ store-commons/autoconfigure/src/main/java/com/asrevo/cvhome/s2s/config/CvhomeSharedConfig.java
@@ -14,14 +14,17 @@ import com.asrevo.cvhome.s2s.config.internal.SwaggerConfig;
 import com.asrevo.cvhome.s2s.config.internal.WebClientServicesConfig;
 import com.asrevo.cvhome.s2s.config.internal.WebConfig;
 import com.asrevo.cvhome.s2s.jwt.IssuerUriSetConfigrationProperties;
+import com.asrevo.cvhome.s2s.model.AdminUserProperties;
 import com.asrevo.cvhome.s2s.model.AppProperties;
 import com.asrevo.cvhome.s2s.model.CdnProperties;
 import com.asrevo.cvhome.s2s.model.CdnStorageProperties;
+import com.asrevo.cvhome.s2s.model.OAuth2ClientProperties;
 import com.asrevo.cvhome.s2s.model.PodInfoProperties;
 import com.asrevo.cvhome.s2s.model.PodProperties;
 import com.asrevo.cvhome.s2s.model.ServiceDomainProperties;
 import com.asrevo.cvhome.s2s.model.StoreProductImageProperties;
 import com.asrevo.cvhome.s2s.model.StripeProperties;
+import com.asrevo.cvhome.s2s.model.TestStoreProperties;
 
 @Configuration
 @Import({EcsInfoConfig.class, SwaggerConfig.class, WebConfig.class, WebClientServicesConfig.class,
@@ -30,7 +33,9 @@ import com.asrevo.cvhome.s2s.model.StripeProperties;
         ServletPermissionConfig.class})
 @EnableConfigurationProperties({PodInfoProperties.class, ServiceDomainProperties.class, AppProperties.class,
         PodProperties.class, CdnProperties.class, StripeProperties.class, StoreProductImageProperties.class,
-        CdnStorageProperties.class, IssuerUriSetConfigrationProperties.class})
+        CdnStorageProperties.class, IssuerUriSetConfigrationProperties.class, TestStoreProperties.class,
+        AdminUserProperties.class, OAuth2ClientProperties.class
+})
 public class CvhomeSharedConfig {
 
 }
