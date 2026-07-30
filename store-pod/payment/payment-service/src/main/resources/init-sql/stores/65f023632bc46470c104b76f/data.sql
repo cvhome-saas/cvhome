0193759eb23f5cce8e874780aabe878d6e8bae91
ashraf <ashraf1abdelrasool@gmail.com>
2026-07-30 16:07:32 +0300

    feat improve payment configuration repository

--- store-pod/payment/payment-service/src/main/resources/init-sql/stores/65f023632bc46470c104b76f/data.sql
+++ store-pod/payment/payment-service/src/main/resources/init-sql/stores/65f023632bc46470c104b76f/data.sql
@@ -1,3 +1,6 @@
--- ORG1-STORE1
-SELECT 1; -- No operation
-
+INSERT INTO payment.payment_configuration (store_merchant_id, payment_type, api_key, secret_key, webhook_secret,
+                                           enabled)
+VALUES ('65f023632bc46470c104b76f', 'STRIPE', 'stripe-app-id', 'stripe-app-secret', 'stripe-webook-secret', true),
+       ('65f023632bc46470c104b76f', 'PAYPAL', 'paypal-app-id', 'paypal-app-secret', 'paypal-webook-secret', true),
+       ('65f023632bc46470c104b76f', 'COD', NULL, NULL, NULL, true),
+       ('65f023632bc46470c104b76f', 'MANUAL_TRANSFER', NULL, NULL, NULL, true);
